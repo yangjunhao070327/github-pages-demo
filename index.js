c040849e33234dabefc276ca95100c62cbd7dbc6
LH <lihua@axhome.com.cn>
2019-12-03 15:52:58 +0800

    更新

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function () {
-  document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+  document.getElementById('main-content').innerHTML = '这是一个需要发布到gitHub pages的项目'
 }
